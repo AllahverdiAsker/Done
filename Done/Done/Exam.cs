31e011807036a678649bcc8fd057fe284d82b4ab
Allahverdi Asgarli <allahverdiaa@code.edu.az>
2022-06-22 01:53:08 -0700

    done

---
+++ Done/Done/Exam.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Done
+{
+    class Exam
+    {
+
+        private string _student;
+        private double _point;
+        private DateTime _startDate;
+        private DateTime _endTime;
+        public string Subject;
+
+        public double Point
+        {
+            get
+            {
+                return _point;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    _point = value;
+                }
+            }
+        }
+        public string Student
+        {
+            get => _student;
+            set
+            {
+                bool check = true;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (!Char.IsLetter(value[i]))
+                    {
+                        check = false;
+
+                    }
+                }
+                if (check && value.Length > 2)
+                {
+                    _student = value;
+                }
+            }
+
+
+        }
+
+
+
+        public Exam(DateTime startdate, DateTime enddate)
+        {
+            if (startdate < enddate)
+            {
+                _startDate = startdate;
+                _endTime = enddate;
+            }
+        }
+
+
+    }
+}
---
+++ Done/Done/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Done
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Exam> exams = new List<Exam>();
+
+            string ac = "";
+
+
+            for (; ; )
+            {
+                Console.WriteLine("Exam Sayi");
+                ac = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(ac))
+                {
+                    break;
+                }
+            }
+
+
+            bool search = int.TryParse(ac, out int inputac);
+
+
+
+            if (search)
+
+            {
+                for (int i = 0; i < inputac; i++)
+                {
+
+                    string aass = "";
+                    for (; ; )
+                    {
+                        Console.Write("Enter Student");
+                        aass = Console.ReadLine();
+
+                        if (!string.IsNullOrWhiteSpace(aass))
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Wrong - Try Again");
+                    }
+
+                    string asss = "";
+                    for (; ; )
+                    {
+                        Console.Write("Subject");
+                        asss = Console.ReadLine();
+
+                        if (!string.IsNullOrWhiteSpace(asss))
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Try Again");
+                    }
+
+                    Console.Write("Point");
+                    string a = "";
+                    double aa;
+                    for (; ; )
+                    {
+
+                        a = Console.ReadLine();
+
+                        if (double.TryParse(a, out aa))
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Try Again");
+                    }
+
+                    string b = "";
+                    DateTime bb;
+                    for (; ; )
+                    {
+                        Console.Write("Exam Begin");
+                        b = Console.ReadLine();
+
+                        if (DateTime.TryParse(b, out bb))
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Try Again");
+                    }
+
+                    string c = "";
+                    DateTime cc;
+                    for (; ; )
+                    {
+                        Console.Write("Exam end");
+
+                        if (DateTime.TryParse(c, out cc) && bb > cc)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Try Again");
+                    }
+
+                    Exam exam = new Exam(bb, cc)
+                    {
+                        Point = aa,
+                        Student = aass,
+                        Subject = asss
+                    };
+
+                    exams.Add(exam);
+                    Console.WriteLine(" Exam Added ");
+
+                }
+
+
+            }
+
+        }
+
+    }
+    
+
+}
